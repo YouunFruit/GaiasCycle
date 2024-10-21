f6c2bbf05c04e73230ed6001be918c85a81c2c3b
MarieBelle88 <137484339+MarieBelle88@users.noreply.github.com>
2024-10-21 16:02:27 +0200

    database query

--- database/query.sql
+++ database/query.sql
@@ -1,2 +1,4 @@
-use gaiacycle;
-select * from Farm;
+SELECT Users.Name, Users.Role, Farm.Location, Farm.Type
+FROM Users
+         JOIN Devices ON Users.UserID = Devices.UserID
+         JOIN Farm ON Devices.FarmID = Farm.FarmID;
